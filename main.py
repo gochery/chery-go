c34989704cd6e05ee650ce4d31ded6609ecfc046
gochery <tu2007ky@hotmail.com>
2025-06-08 18:21:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
-import os
+import logging
 from telegram import Update
 from telegram.ext import Application, MessageHandler, ContextTypes, filters
 
-API_TOKEN = "7560777141:AAGTOemLV2nO5U7wt9bqhnfDdj43NHdzV4c"
+TOKEN = "7560777141:AAGTOemLV2nO5U7wt9bqhnfDdj43NHdzV4c"
+
+logging.basicConfig(level=logging.INFO)
 
 async def handle_pdf(update: Update, context: ContextTypes.DEFAULT_TYPE):
     document = update.message.document
     if document and document.mime_type == "application/pdf":
-        file_id = document.file_id
-        await update.message.reply_text(f"file_id: `{file_id}`", parse_mode="Markdown")
+        await update.message.reply_text(
+            f"📄 file_id:\n`{document.file_id}`",
+            parse_mode="Markdown"
+        )
 
-app = Application.builder().token(API_TOKEN).build()
+app = Application.builder().token(TOKEN).build()
 app.add_handler(MessageHandler(filters.Document.PDF, handle_pdf))
 
 if __name__ == "__main__":
